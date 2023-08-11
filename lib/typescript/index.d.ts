8e37529f04c3d42e06a1b7d2f85b63ae30027f72
shomat zuhry <shomatz99@gmail.com>
2023-08-11 18:22:23 +0700

    build: 0.1.2

---
+++ lib/typescript/index.d.ts
@@ -0,0 +1,18 @@
+export type Region = {
+    latitude: number;
+    longitude: number;
+    latitudeDelta: number;
+    longitudeDelta: number;
+};
+export type MapItem = {
+    title: string;
+    subtitle: string;
+    latitude: number;
+    longitude: number;
+    phoneNumber: string;
+    name: string;
+    url: string;
+};
+export declare function multiply(a: number, b: number): Promise<number>;
+export declare function search(query: string, region: Region): Promise<any>;
+//# sourceMappingURL=index.d.ts.map
